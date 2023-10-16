f91646382c9c99ab344ae600db9e5d00efc0f259
Dell <Dell@DESKTOP-9JBOCDG>
2023-10-16 13:42:13 -0400

    test1

--- src/main/java/com/mycompany/proyecto1/Proyecto1.java
+++ src/main/java/com/mycompany/proyecto1/Proyecto1.java
@@ -11,6 +11,7 @@ package com.mycompany.proyecto1;
 public class Proyecto1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hola mundo");
+        System.out.println("pupu");
     }
 }
