e3afcc794c8dde7f03d7d3df419cfc94cb4daccb
Dell <Dell@DESKTOP-9JBOCDG>
2023-10-20 17:02:53 -0400

    añadido modificacion de grafos

--- src/main/java/classes/ModificarGrafosInterfaz.java
+++ src/main/java/classes/ModificarGrafosInterfaz.java
@@ -26,21 +26,52 @@ public class ModificarGrafosInterfaz extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanel1 = new javax.swing.JPanel();
+        TituloModGrafos = new javax.swing.JLabel();
+        AñadirUsuarioLab = new javax.swing.JLabel();
+        TextAñadirUsuario = new javax.swing.JTextField();
+        EliminarUsuarioLab = new javax.swing.JLabel();
+        TextEliminarUsuario = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 170, -1, -1));
+
+        TituloModGrafos.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        TituloModGrafos.setForeground(new java.awt.Color(255, 255, 255));
+        TituloModGrafos.setText("Modificación de grafos");
+        getContentPane().add(TituloModGrafos, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 10, 270, 50));
+
+        AñadirUsuarioLab.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        AñadirUsuarioLab.setForeground(new java.awt.Color(255, 255, 255));
+        AñadirUsuarioLab.setText("Añadir usuario (identificar relaciones con otros usuarios):");
+        getContentPane().add(AñadirUsuarioLab, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 120, 530, 60));
+
+        TextAñadirUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TextAñadirUsuarioActionPerformed(evt);
+            }
+        });
+        getContentPane().add(TextAñadirUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 170, 630, 70));
+
+        EliminarUsuarioLab.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        EliminarUsuarioLab.setForeground(new java.awt.Color(255, 255, 255));
+        EliminarUsuarioLab.setText("Eliminar un usuario:");
+        getContentPane().add(EliminarUsuarioLab, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 280, 190, 50));
+        getContentPane().add(TextEliminarUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 340, 620, 60));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/classes/fondoGrafosOperations.png"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 980, 510));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void TextAñadirUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TextAñadirUsuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TextAñadirUsuarioActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +109,12 @@ public class ModificarGrafosInterfaz extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel AñadirUsuarioLab;
+    private javax.swing.JLabel EliminarUsuarioLab;
+    private javax.swing.JTextField TextAñadirUsuario;
+    private javax.swing.JTextField TextEliminarUsuario;
+    private javax.swing.JLabel TituloModGrafos;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
